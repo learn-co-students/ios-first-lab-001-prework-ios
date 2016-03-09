755bf1b7e6a50ffab2b65d967036d019ea4ca12d
anasmilhem <anasmilhem1997@gmail.com>
2016-03-09 01:12:35 -0500

    edit FISappdelegate

--- my-first-lab/FISAppDelegate.m
+++ my-first-lab/FISAppDelegate.m
@@ -8,6 +8,7 @@
 // Add two slashes in front of line 24.
 // Remove two slashes from line 25.
 // Press Command-U to run the tests!
+// there is no line 24 and 25
 // /////////////////////////////////////////////////////
 - (NSString *)didItWork
 {
