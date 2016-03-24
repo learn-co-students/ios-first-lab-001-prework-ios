3fc78838dac433afcb169de67cd93599ca2cbb23
priscim <priscim@aol.com>
2016-03-24 18:50:41 -0400

    My first commit

--- my-first-lab/FISAppDelegate.m
+++ my-first-lab/FISAppDelegate.m
@@ -7,12 +7,12 @@
 // //////////////////////////////////////////////////////
 // Add two slashes in front of line 24.
 // Remove two slashes from line 25.
-// Press Command-U to run the tests!
+// Press Command-U to run the tests
 // /////////////////////////////////////////////////////
 - (NSString *)didItWork
 {
-    return nil;
-//    return @"YES";
+//    return nil;
+   return @"YES";
 }
 
 @end
